d9948e244bcdb943b91a9a000d0b9dcab480efc3
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-12 01:38:46 +0530

    Updated the code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="underline">App</div>;
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={`${darkTheme ? "dark" : ""}`}>
+      <div className="dark:bg-gray-900 dark:text-gray-200 bg-gray-100 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;

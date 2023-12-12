eaaaf377a0718b85d7899ed2362e977c75ea7dbb
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-12 19:37:26 +0530

    Updated the footer with styling and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+      2023 Google, Inc.
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;

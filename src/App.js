71050a979633da3ca8be30ff8597423290e2bfb3
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-12 23:24:56 +0530

    Added new styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import RoutesComponent from "./components/Routes";
+import RoutesComponent from "./components/RoutesComponent";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className={`${darkTheme ? "dark" : ""}`}>
-      <div className="dark:bg-gray-900 dark:text-gray-200 bg-gray-100 min-h-screen">
+      <div className="dark:bg-gray-900 dark:text-gray-200 bg-indigo-50 min-h-screen">
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <RoutesComponent />
         <Footer />

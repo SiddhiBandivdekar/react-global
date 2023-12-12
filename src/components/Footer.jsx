5143375911932653627b7e3f84827ad022f3876f
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-13 00:51:45 +0530

    Changed the name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
-      2023 Google, Inc.
+      2023 Global, Inc.
     </div>
   );
 };

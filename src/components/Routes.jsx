5f378d729a2753c010aa8f6c65def05ab30ba1c7
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-12 19:34:56 +0530

    Forwarding components to Results component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Routes, redirect } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Results from "./Results";
 
 const RoutesComponent = () => {
-  return <div className="p-4">RoutesComponent</div>;
+  return (
+    <div className="p-4">
+      <Routes>
+        <Route path="/" element={<Navigate to="/search" replace />} />
+
+        <Route path="/search" element={<Results />} />
+        <Route path="/images" element={<Results />} />
+        <Route path="/news" element={<Results />} />
+        <Route path="/videos" element={<Results />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default RoutesComponent;

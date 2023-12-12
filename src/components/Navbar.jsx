26f4427dcecc9f13cf0eca9578ecc8f4f2147114
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-12 23:25:18 +0530

    Changed the color and text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = ({ darkTheme, setDarkTheme }) => {
     >
       <div className="flex justify-between items-center space-x-5 w-screen">
         <Link to="/">
-          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
-            Google
+          <p className="text-2xl bg-rose-800 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+            Global
           </p>
         </Link>
         <button
           type="button"
           onClick={() => setDarkTheme(!darkTheme)}
-          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
+          className="text-l font-bold dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-3 py-1 hover:shadow-lg"
         >
-          {darkTheme ? "Light Mode" : "Dark Mode"}
+          {darkTheme ? "Light ☀︎" : "Dark ☽"}
         </button>
       </div>
       <Search />
